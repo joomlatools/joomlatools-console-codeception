b77721b603039315fe3429e2b73d160d44069494
yiendos <yiendos@sbmatrix.com>
2018-04-20 14:01:56 +0100

    getting check method to work

--- Joomlatools/Console/Command/Codeception.php
+++ Joomlatools/Console/Command/Codeception.php
@@ -47,14 +47,14 @@ class Codeception extends AbstractSite
     {
         parent::execute($input, $output);
 
-        $this->check($input, $output);
-
         $path                       = self::$files;
         $this->config               = $path . DIRECTORY_SEPARATOR . 'codeception.yml';
         $this->check_host_script    = $path . DIRECTORY_SEPARATOR . 'check_host_machine_requirements.sh';
         $this->tests                = $path . DIRECTORY_SEPARATOR . 'tests';
         $this->dest                 = $this->target_dir . DIRECTORY_SEPARATOR;
         $this->tests_dest           = $this->dest . 'tests';
+
+        $this->check($input, $output);
 
         //lets copy over the original files and folders
         `cp $this->config $this->dest`;
@@ -75,7 +75,7 @@ class Codeception extends AbstractSite
 
         $output->writeLn('<info>Codeception project created.</info>');
         $output->writeLn('<comment>We suggested you run the following bash script, as this will check your host machine to see you are set up correctly for codeception tests:</comment>');
-        $output->writeLn( 'www.' . DIRECTORY_SEPARATOR . $this->site . DIRECTORY_SEPARATOR . "check_host_machine_requirements.sh");
+        $output->writeLn( 'www' . DIRECTORY_SEPARATOR . $this->site . DIRECTORY_SEPARATOR . "check_host_machine_requirements.sh");
 
         $output->writeLn('<info>After that you can run your tests at any time:</info>');
         $output->writeLn('codecept run acceptance');
@@ -88,15 +88,14 @@ class Codeception extends AbstractSite
 
         if (file_exists($configuration))
         {
-            `rm $configuration`;
+            `rm $this->configuration`;
             //throw new \RuntimeException('Codeception is already installed');
             //return;
         }
 
         if (is_dir($tests))
         {
-
-            `rm -R $tests`;
+            `rm -R -f $tests`;
             //throw new \RuntimeException('Codeception tests folder already exists');
             //return;
         }
